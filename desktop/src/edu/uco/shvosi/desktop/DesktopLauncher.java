81e6e5dfd18f6cade0c92cd8b8e89cf857e5af25
CaryStringfield <captainclark@cox.net>
2015-09-17 05:02:16 -0500

    Rebuilt start page and integrated into game.  Set up game state templates. Other minor changes

--- desktop/src/edu/uco/shvosi/desktop/DesktopLauncher.java
+++ desktop/src/edu/uco/shvosi/desktop/DesktopLauncher.java
@@ -7,8 +7,9 @@ import edu.uco.shvosi.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+                config.useGL30 = false;
                 config.title = "Get Out of My Lair!";
-                config.width = 800;
+                config.width = 810;
                 config.height = 480;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
