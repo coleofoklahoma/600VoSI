6472fcab6293b64c414aca056b843969c5bcfd2d
CaryStringfield <captainclark@cox.net>
2015-10-07 23:50:02 -0500

    add class MeleeAttack

--- core/src/edu/uco/shvosi/MeleeAttack.java
+++ core/src/edu/uco/shvosi/MeleeAttack.java
@@ -1,47 +1,84 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.uco.shvosi;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-/**
- *
- * @author cody
- */
-class MeleeAttack extends Skill {
+public class MeleeAttack extends Entity implements Observer {
 
+    private int state = 0;
+    private float elapsedAttack;
+    private boolean activateAttack;
     private TextureRegion temp;
-    
-    public MeleeAttack(int x, int y, Animation mainAnimation, int damage) {
-        super(x, y, mainAnimation);
-        this.damage = damage;
+    private Animation meleeAttack;
+    private int damage;
+   // Sound meleeSound = Gdx.audio.newSound(Gdx.files.internal("sounds/trap1.mp3"));
+
+    public MeleeAttack(int cX, int cY) {
+        super(Constants.EntityGridCode.TRAP, TextureLoader.MELEETEXTURE, cX, cY);
+        meleeAttack = TextureLoader.meleeAttack;
+        activateAttack = false;
+        elapsedAttack = 0f;
+        this.damage = 25;
+        this.setVisible(false);
+        this.state = 0;
     }
 
     @Override
-    public void draw(Batch batch, float alpha, Protagonist bernard) {
-        update();
-        
-            if (((Entity) bernard).getTextureRegion().isFlipX()) {
-            setX(bernard.getX() - Constants.TILEDIMENSION * 3);
-            setY(bernard.getY());
-            temp = mainAnimation.getKeyFrame(skillRunTime);
-            boundingBox.set(getX() - Constants.TILEDIMENSION * 4, getY(), temp.getRegionWidth(), temp.getRegionHeight());
-            temp.flip(true, false);
-            batch.draw(temp, getX(), getY(), Constants.TILEDIMENSION * 3, Constants.TILEDIMENSION);
-            temp.flip(true, false);
-        } else {
-            setX(bernard.getX() + Constants.TILEDIMENSION);
-            setY(bernard.getY());
-            boundingBox.set(getX(), getY(), Constants.TILEDIMENSION * 3, Constants.TILEDIMENSION);
-            batch.draw(mainAnimation.getKeyFrame(skillRunTime), getX(), getY(), Constants.TILEDIMENSION * 3, Constants.TILEDIMENSION);
+    public void draw(Batch batch, float alpha) {
+        super.draw(batch, alpha);
+        if (activateAttack) {
+            elapsedAttack += Gdx.graphics.getDeltaTime();
+            temp = meleeAttack.getKeyFrame(elapsedAttack);
+            batch.draw(meleeAttack.getKeyFrame(elapsedAttack), this.getX(), this.getY(), Constants.TILEDIMENSION, Constants.TILEDIMENSION);
+            if (meleeAttack.isAnimationFinished(elapsedAttack)) {
+                activateAttack = false;
+                elapsedAttack = 0f;
+                this.state = 1;               
+                this.setHealth(0);
+            }
         }
- 
-
     }
 
+    @Override
+    public void collision(Entity entity) {
+    }
+
+    public void observerUpdate(Object o) {
+        if (o instanceof Protagonist) {
+            Protagonist bernard = (Protagonist) o;
+            Integer xCoordinate = bernard.getDCX();
+            Integer yCoordinate = bernard.getDCY();
+
+            if (xCoordinate == this.getCX() && yCoordinate == this.getCY() && this.state == 0) {
+                this.setVisible(true);
+                this.activateAttack = true;
+                //meleeSound.play(1.0f);
+                if (bernard.getShieldFlag() == 1) {
+                    bernard.setImage(TextureLoader.BERNARDTEXTURE);
+                    bernard.setShieldFlag(0);
+                } else {
+                    if (bernard.getExecuteBarrier() == true) {
+                        bernard.setHealth(bernard.getHealth() - this.damage / 2);
+                        bernard.setBarrierDamage(this.damage / 2);
+                        bernard.setBarrierLimit(bernard.getBarrierLimit() - 1);
+                    } else {
+                        bernard.setHealth(bernard.getHealth() - this.damage);
+                    }
+                    
+                    if (bernard.getHealth() < 1) {
+                        bernard.setHealth(0);
+                    }
+                }
+            }
+            if (bernard.getExecuteDetection() == true && bernard.getDetectionCollisionBox().intersects(this.getCX(), this.getCY(), 3, 3)) {
+                this.setVisible(true);
+            }
+        }
+    }
 }
